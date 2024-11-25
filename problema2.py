69a898fea83bda0e5218a52387851a1c59fcbdc9
Laura Daniela <lamarinb@unal.edu.co>
2024-11-25 15:50:46 -0500

    ejercicioderepaso1

--- problema2.py
+++ problema2.py
@@ -1,2 +1,22 @@
-carrera = "Ingeniería electrónica"
-print (type(carrera))
+combo1 = "a"
+combo2 = "b"
+combo3 = "c"
+print("Bienvenido! Escoja la cantidad de productos que desee comprar: ")
+print("\n")
+print("a. Gomitas trululu = 2000")
+print("b. Chocolatina jet 1000")
+print("c. Almendras de chocolate 5000")
+def descuento(x,y)
+    return = 
+if valor == combo1:
+    print("El descuento es del 5%")
+elif valor == combo2:
+    print("El descuento es del 10%")
+elif valor == combo3:
+    print("El descuento es del 20%")
+
+
+
+
+
+    
--- problemagit.py
+++ problemagit.py
@@ -1,7 +1,14 @@
-##
-edad = 17
-
-if edad == 17:
-    print("su edad es correcta")
-else:
-    print("incorrecto")
+import math
+r = int((input("Ingrese el radio para calcular el área, perímetro y volumen:")))
+def area(r):
+    area_circulo = math.pi * r ** 2
+    return area_circulo
+def perimetro(r):
+    perimetro_circulo = math.pi * (2*r)
+    return perimetro_circulo
+def volumen(r):
+    volumen_esfera = (4/3) * math.pi * r ** 3
+    return volumen_esfera
+print("Su área es: ",area(r))
+print("Su perímetro es: ",perimetro(r))
+print("Su volumen es: ",volumen(r))
